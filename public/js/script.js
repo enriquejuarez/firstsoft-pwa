45420b826b6db48d3e249cdca8e2e2f9b0f1560e
enriquejuarez <caenjuji@gmail.com>
2018-08-19 15:13:43 -0500

    Primera versión de firstsoft pwa

---
+++ public/js/script.js
@@ -0,0 +1,351 @@
+$(document).ready(function() {
+
+	// OWL CAROUSEL INSTALLATION
+	$("#testimonial-carousel").owlCarousel({
+		items:1,
+		itemsDesktop : [1000,1], //5 items between 1000px and 901px
+      	itemsDesktopSmall : [900,1], // betweem 900px and 601px
+      	itemsTablet: [600,1],
+		itemsMobile :[479,1],
+		pagination:true
+	});
+	$("#home-slider").owlCarousel({
+		items:1,
+		itemsDesktop : [1000,1], //5 items between 1000px and 901px
+      	itemsDesktopSmall : [900,1], // betweem 900px and 601px
+      	itemsTablet: [600,1],
+		itemsMobile :[479,1],
+		pagination:false,
+		navigation:true,
+		navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"]
+
+	});
+
+	/* Navigation Menu*/
+	var offsettop = $('.navbar').offset().top;
+	if (offsettop > 50) {
+        $('.navbar').addClass('colored-nav');
+        $('.navbar').addClass('gradient-red');
+        $("#scroll-top-div").fadeIn('500');
+    } else {
+        $('.navbar').removeClass('colored-nav');
+        $('.navbar').removeClass('gradient-red');
+        $("#scroll-top-div").fadeOut('500');
+    }
+	var num = 50; //number of pixels before modifying styles
+
+	$(window).bind('scroll', function () {
+	    if ($(window).scrollTop() > num) {
+	        $('.navbar').addClass('colored-nav');
+	        $('.navbar').addClass('gradient-red');
+	        $("#scroll-top-div").fadeIn('500');
+	    } else {
+	        $('.navbar').removeClass('colored-nav');
+	        $('.navbar').removeClass('gradient-red');
+	        $("#scroll-top-div").fadeOut('500');
+	    }
+	});
+
+
+
+	/* SMOOTH SCROLLING SCRIPT*/
+	// Add smooth scrolling to all links
+	$(".navbar-nav li a").on('click', function(event) {
+
+		// Make sure this.hash has a value before overriding default behavior
+		if (this.hash !== "") {
+			// Prevent default anchor click behavior
+			event.preventDefault();
+
+			// Store hash
+			var hash = this.hash;
+
+			// Using jQuery's animate() method to add smooth page scroll
+			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function(){
+
+				// Add hash (#) to URL when done scrolling (default click behavior)
+				window.location.hash = hash;
+			});
+		} // End if
+	});
+
+	/****************************BACK TO TOP************************************/
+	$('#scroll-top-div').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
+
+});
+
+jQuery(document).ready(function($){
+    //set animation timing
+    var animationDelay = 2500,
+        //loading bar effect
+        barAnimationDelay = 3800,
+        barWaiting = barAnimationDelay - 3000, //3000 is the duration of the transition on the loading bar - set in the scss/css file
+        //letters effect
+        lettersDelay = 50,
+        //type effect
+        typeLettersDelay = 150,
+        selectionDuration = 500,
+        typeAnimationDelay = selectionDuration + 800,
+        //clip effect
+        revealDuration = 600,
+        revealAnimationDelay = 1500;
+
+    initHeadline();
+
+
+    function initHeadline() {
+        //insert <i> element for each letter of a changing word
+        singleLetters($('.cd-headline.letters').find('b'));
+        //initialise headline animation
+        animateHeadline($('.cd-headline'));
+    }
+
+    function singleLetters($words) {
+        $words.each(function(){
+            var word = $(this),
+                letters = word.text().split(''),
+                selected = word.hasClass('is-visible');
+            for (i in letters) {
+                if(word.parents('.rotate-2').length > 0) letters[i] = '<em>' + letters[i] + '</em>';
+                letters[i] = (selected) ? '<i class="in">' + letters[i] + '</i>': '<i>' + letters[i] + '</i>';
+            }
+            var newLetters = letters.join('');
+            word.html(newLetters).css('opacity', 1);
+        });
+    }
+
+    function animateHeadline($headlines) {
+        var duration = animationDelay;
+        $headlines.each(function(){
+            var headline = $(this);
+
+            if(headline.hasClass('loading-bar')) {
+                duration = barAnimationDelay;
+                setTimeout(function(){ headline.find('.cd-words-wrapper').addClass('is-loading') }, barWaiting);
+            } else if (headline.hasClass('clip')){
+                var spanWrapper = headline.find('.cd-words-wrapper'),
+                    newWidth = spanWrapper.width() + 10
+                spanWrapper.css('width', newWidth);
+            } else if (!headline.hasClass('type') ) {
+                //assign to .cd-words-wrapper the width of its longest word
+                var words = headline.find('.cd-words-wrapper b'),
+                    width = 0;
+                words.each(function(){
+                    var wordWidth = $(this).width();
+                    if (wordWidth > width) width = wordWidth;
+                });
+                headline.find('.cd-words-wrapper').css('width', width);
+            };
+
+            //trigger animation
+            setTimeout(function(){ hideWord( headline.find('.is-visible').eq(0) ) }, duration);
+        });
+    }
+
+    function hideWord($word) {
+        var nextWord = takeNext($word);
+
+        if($word.parents('.cd-headline').hasClass('type')) {
+            var parentSpan = $word.parent('.cd-words-wrapper');
+            parentSpan.addClass('selected').removeClass('waiting');
+            setTimeout(function(){
+                parentSpan.removeClass('selected');
+                $word.removeClass('is-visible').addClass('is-hidden').children('i').removeClass('in').addClass('out');
+            }, selectionDuration);
+            setTimeout(function(){ showWord(nextWord, typeLettersDelay) }, typeAnimationDelay);
+
+        } else if($word.parents('.cd-headline').hasClass('letters')) {
+            var bool = ($word.children('i').length >= nextWord.children('i').length) ? true : false;
+            hideLetter($word.find('i').eq(0), $word, bool, lettersDelay);
+            showLetter(nextWord.find('i').eq(0), nextWord, bool, lettersDelay);
+
+        }  else if($word.parents('.cd-headline').hasClass('clip')) {
+            $word.parents('.cd-words-wrapper').animate({ width : '2px' }, revealDuration, function(){
+                switchWord($word, nextWord);
+                showWord(nextWord);
+            });
+
+        } else if ($word.parents('.cd-headline').hasClass('loading-bar')){
+            $word.parents('.cd-words-wrapper').removeClass('is-loading');
+            switchWord($word, nextWord);
+            setTimeout(function(){ hideWord(nextWord) }, barAnimationDelay);
+            setTimeout(function(){ $word.parents('.cd-words-wrapper').addClass('is-loading') }, barWaiting);
+
+        } else {
+            switchWord($word, nextWord);
+            setTimeout(function(){ hideWord(nextWord) }, animationDelay);
+        }
+    }
+
+    function showWord($word, $duration) {
+        if($word.parents('.cd-headline').hasClass('type')) {
+            showLetter($word.find('i').eq(0), $word, false, $duration);
+            $word.addClass('is-visible').removeClass('is-hidden');
+
+        }  else if($word.parents('.cd-headline').hasClass('clip')) {
+            $word.parents('.cd-words-wrapper').animate({ 'width' : $word.width() + 10 }, revealDuration, function(){
+                setTimeout(function(){ hideWord($word) }, revealAnimationDelay);
+            });
+        }
+    }
+
+    function hideLetter($letter, $word, $bool, $duration) {
+        $letter.removeClass('in').addClass('out');
+
+        if(!$letter.is(':last-child')) {
+            setTimeout(function(){ hideLetter($letter.next(), $word, $bool, $duration); }, $duration);
+        } else if($bool) {
+            setTimeout(function(){ hideWord(takeNext($word)) }, animationDelay);
+        }
+
+        if($letter.is(':last-child') && $('html').hasClass('no-csstransitions')) {
+            var nextWord = takeNext($word);
+            switchWord($word, nextWord);
+        }
+    }
+
+    function showLetter($letter, $word, $bool, $duration) {
+        $letter.addClass('in').removeClass('out');
+
+        if(!$letter.is(':last-child')) {
+            setTimeout(function(){ showLetter($letter.next(), $word, $bool, $duration); }, $duration);
+        } else {
+            if($word.parents('.cd-headline').hasClass('type')) { setTimeout(function(){ $word.parents('.cd-words-wrapper').addClass('waiting'); }, 200);}
+            if(!$bool) { setTimeout(function(){ hideWord($word) }, animationDelay) }
+        }
+    }
+
+    function takeNext($word) {
+        return (!$word.is(':last-child')) ? $word.next() : $word.parent().children().eq(0);
+    }
+
+    function takePrev($word) {
+        return (!$word.is(':first-child')) ? $word.prev() : $word.parent().children().last();
+    }
+
+    function switchWord($oldWord, $newWord) {
+        $oldWord.removeClass('is-visible').addClass('is-hidden');
+        $newWord.removeClass('is-hidden').addClass('is-visible');
+    }
+});
+
+/*Animation typewriter style using css steps()*/
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+
+        // array with texts to type in typewriter
+        var dataText = [ "Laravel.", "Vue.js.", "Android.", "iOS.", "Ionic."];
+
+        // type one text in the typwriter
+        // keeps calling itself until the text is finished
+        function typeWriter(text, i, fnCallback) {
+        // chekc if text isn't finished yet
+        if (i < (text.length)) {
+          // add next character to h1
+         document.querySelector("h2").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+
+          // wait for a while and call this function again for next character
+          setTimeout(function() {
+            typeWriter(text, i + 1, fnCallback)
+          }, 100);
+        }
+        // text finished, call callback if there is a callback function
+        else if (typeof fnCallback == 'function') {
+          // call callback after timeout
+          setTimeout(fnCallback, 700);
+        }
+        }
+        // start a typewriter animation for a text in the dataText array
+        function StartTextAnimation(i) {
+         if (typeof dataText[i] == 'undefined'){
+            setTimeout(function() {
+              StartTextAnimation(0);
+            }, 20000);
+         }
+         // check if dataText[i] exists
+        if (i < dataText[i].length) {
+          // text exists! start typewriter animation
+         typeWriter(dataText[i], 0, function(){
+           // after callback (and whole text has been animated), start next text
+           StartTextAnimation(i + 1);
+         });
+        }
+        }
+        // start the text animation
+        StartTextAnimation(0);
+    };
+
+/**************************************PWA****************************************************************/
+((d, w, n, c) => {
+    //Registro sw
+    if ('serviceWorker' in n){
+        w.addEventListener('load', ()=>{
+            n.serviceWorker.register('./sw.js')
+                .then(registration => {
+                    c('Service Worker registrado con éxito', registration.scope)
+                })
+                .catch(err => c('Registro de Serive Worker fallido', err))
+        })
+    }
+})(document, window, navigator, console.log);
---
+++ public/sw.js
@@ -0,0 +1,129 @@
+
+const CACHE_NAME = 'firstsoft-pwa-cache-v1',
+	urlsToCache = [
+		'/',
+		'./',
+		'./?utm=homescreen',
+		'./index.html',
+		'./css/style.css',
+		'./js/script.js',
+		'./sw.js',
+		'./favicon.png',
+		'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
+	]
+
+self.addEventListener('install', e => {
+	console.log('Evento: SW Instalado')
+	e.waitUntil(
+		caches.open(CACHE_NAME)
+			.then(cache => {
+				console.log('Archivos en cache')
+				return cache.addAll(urlsToCache)
+				.then(() => self.skipWaiting())
+				//skipWaiting forza a SW a activarse
+			})
+			.catch(err => console.log('Fallo registro de cache', err))
+	)
+})
+
+self.addEventListener('activate', e => {
+	console.log('Evento: SW Activado')
+	const cacheList  = [CACHE_NAME]
+	e.waitUntil(
+		caches.keys()
+			.then(cachesName => {
+				return Promise.all(
+					cachesName.map(cacheName => {
+						//Se elimina lo que ya no se necesita en cache
+						if (cacheList.indexOf(cacheName) === -1){
+							return caches.delete(cacheName)
+						}
+					})
+				)
+			})
+			.then(() => {
+				console.log('El cache esta limpio y actualizado')
+				// Le indica al SW activar el cache actual
+				return self.clients.claim()
+			})
+			.catch()
+	)
+})
+
+self.addEventListener('fetch', e => {
+	console.log('Evento: SW Recuperando')
+	e.respondWith(
+		caches.match(e.request)
+		.then(res => {
+			if (res){
+				return res
+			}
+			return fetch(e.request)
+				.then(res => {
+					let resToCache = res.clone()
+					caches.open(CACHE_NAME)
+						.then(cache => {
+							cache
+								.put(e.request, resToCache)
+								.catch(err => console.log(`${e.request.url}: ${err.message}`))
+						})
+						return res
+				})
+		})
+	)
+})
+
+self.addEventListener('push', e => {
+	console.log('Evento: Push')
+
+	const title = 'Push Notification Demo'
+	const options = {
+		body: 'Click para regresar a la aplicación',
+		icon: './image/icons/icon_192x192.png',
+		//vibrate: [100, 50, 100],
+		//data: { id:1 }
+		// actions: [
+		// 	{
+		// 		'action': 'Si', 'title': 'Amo esta aplicacion', icon: './img/icon_192x192.png'
+		// 	},
+		// 	{
+		// 		'action': 'No', 'title': 'No me gusta esta aplicacion', icon: './img/icon_192x192.png'
+		// 	}
+		// ]
+	}
+	e.waitUntil(self.registration.showNotification(title, options))
+})
+
+self.addEventListener('notificationclick', e => {
+	console.log(e)
+
+	if(e.action === 'Si'){
+		console.log('Amo esta aplicacion')
+		clients.openWindow('http://ed.team')
+	}else if (e.action === 'No'){
+		console.log('No me gusta esta aplicacion')
+	}
+	e.notification.close()
+})
+
+self.addEventListener('sync', e => {
+	console.log('Evento: Sincronizacion de Fondo', e)
+
+	//Revisar que la etiqueta de sincronizacion sea la definida o la que emulan los devtools
+	if (e.tag === 'github' || e.tag === 'test-tag-from-devtools'){
+		e.waitUntil(
+			self.clients.matchAll()
+				.then( all => {
+					return all.map(client => {
+						return client.postMessage('online')
+					})
+				})
+				.catch(err => console.log(err))
+		)
+	}
+})
+
+// self.addEventListener('message', e => {
+// 	console.log('Desde la sincronizacion de Fondo', e.data)
+// 	fetchGitHubuser(localStorage.getItem('github'), true)
+// })
